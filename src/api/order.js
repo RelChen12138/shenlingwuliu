8752ca993122e959b1ef2eb202dee628056b62d3
jiangshan <1439689147@qq.com>
2023-06-08 02:29:17 +0800

    完成

--- src/api/order.js
+++ src/api/order.js
@@ -8,10 +8,14 @@
 import { createAPI } from '@/utils/request'
 
 // 获取订单分页数据
-export const orderList = data => createAPI(`/order-manager/order/page`, 'post', data)
+export const orderList = data =>
+  createAPI(`/order-manager/order/page`, 'post', data)
 // 获取订单详情数据
-export const orderDetail = data => createAPI(`/order-manager/order/${data}`, 'get', data)
+export const orderDetail = data =>
+  createAPI(`/order-manager/order/${data}`, 'get', data)
 // 更新订单
-export const updateOrder = (id, data) => createAPI(`/order-manager/order/${id}`, 'post', data)
+export const updateOrder = (id, data) =>
+  createAPI(`/order-manager/order/${id}`, 'post', data)
 // 获取订单轨迹
-export const getOrderTrackApi = (id) => createAPI(`transport-order-manager/track/${id}`, 'get')
+export const getOrderTrackApi = id =>
+  createAPI(`transport-order-manager/track/${id}`, 'get')
