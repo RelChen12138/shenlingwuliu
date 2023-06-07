4745f641271880efdf13b52943c4d381ba7e8ca7
李燕豪 <12816802+BIG--li@user.noreply.gitee.com>
2023-06-07 21:22:52 +0800

    开始了头部搜索的功能

--- src/views/transit/driver.vue
+++ src/views/transit/driver.vue
@@ -1,28 +1,76 @@
-<!-- 车辆管理 -->
 <template>
-  <div class="dashboard-container driver customer-list-box">
-
+  <div class="driver">
+    <el-card shadow="never">
+      <el-form
+        label-position="left"
+        :model="form"
+      >
+        <el-col :span="8">
+          <el-form-item
+            prop="number"
+            label="司机账号:"
+          >
+            <el-input
+              inline="true"
+              style="width:203px;"
+              type="search"
+              placeholder="请输入司机账号"
+            ></el-input>
+          </el-form-item>
+          <el-form-item
+            prop="jigou"
+            label="所属机构:"
+          >
+            <el-select placeholder="请选择所属机构"></el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item
+            prop="name"
+            label="司机姓名:"
+          >
+            <el-input
+              inline="true"
+              style="width:203px;"
+              type="search"
+              placeholder="请输入司机姓名"
+            ></el-input>
+          </el-form-item>
+          <div>
+            <el-button
+              type="primary"
+              size="medium"
+            >搜索</el-button>
+            <el-button size="medium">重置</el-button>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item
+            prop="pnumber"
+            label="司机手机号:"
+          >
+            <el-input
+              inline="true"
+              style="width:203px;"
+              type="search"
+              placeholder="请输入司机手机号"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+      </el-form>
+    </el-card>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'Courses'
+  data() {
+    return {
+      form: {}
+    }
+  }
 }
 </script>
-<style rel="stylesheet/scss" lang="scss" scoped>
-.alert {
-  margin: 10px 0px;
-}
-.pagination {
-  margin-top: 40px;
-}
-// 下拉框
-/deep/ .el-select {
-  width: 100%;
-}
 
-/deep/ .el-card{
-  overflow: initial;
-}
+<style lang="scss" scoped>
 </style>
-
