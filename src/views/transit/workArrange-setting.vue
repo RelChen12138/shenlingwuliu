74b9916504b539a6226b675f9aee53af6775d71a
itlaozhang <1129359844@qq.com>
2023-06-11 10:57:36 +0800

    修改BUG成功 排班

--- src/views/transit/workArrange-setting.vue
+++ src/views/transit/workArrange-setting.vue
@@ -168,7 +168,7 @@
                 start-placeholder="开始时间"
                 end-placeholder="结束时间"
                 placeholder="选择时间范围"
-                @change="changeTime()"
+                @change="changeTime"
               >
               </el-time-picker>
             </el-form-item>
